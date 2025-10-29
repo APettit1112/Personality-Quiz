63368094160d1220ac67e20716a070cb7d1c99ad
Anslie Pettit <apettit1112@gmail.com>
2025-10-28 19:25:00 -0700

    step 8 of toggling and buttons

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log("script.js connected!");
+
+const questionBlocks = document.querySelectorAll('.question-block');
+const userAnswers = {};
+
+questionBlocks.forEach((block, index) => {
+  const buttons = block.querySelectorAll('.answer-btn');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      buttons.forEach(btn => btn.classList.remove('selected'));
+
+      button.classList.add('selected');
+
+      userAnswers[index + 1] = button.dataset.element;
+      console.log(userAnswers);
+    });
+  });
+});
